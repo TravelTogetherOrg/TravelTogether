7ed184455f893d0e7c4bb80e7e245a774ce63e0d
choyool <rlachdbf@gmail.com>
2023-06-25 21:42:07 +0900

    boardList and boardWrite view 작업

--- WebContent/board.js
+++ WebContent/board.js
@@ -1,11 +1,39 @@
-let col = document.getElementsByClassName("coL");
-let middleHover = document.getElementsByClassName("middleHover");
+/* boardList */
+let col = document.getElementsByClassName('col');
+let middleHover = document.getElementsByClassName('middleHover');
+let thumnail = document.getElementsByClassName('thumnail');
 
 for(let i=0;i<col.length;i++){
-    col[i].addEventListener("mouseover", function(event){
+    col[i].addEventListener('mouseover', function(event){
         middleHover[i].style.display = 'block';
+        thumnail[i].style.transform = 'scale(1.2)';
+        thumnail[i].style.transition = 'all 0.5s';
+        
+
     })
     col[i].addEventListener("mouseout", function(event){
         middleHover[i].style.display = 'none';
+        thumnail[i].style.transform = 'scale(1)';
+        thumnail[i].style.transition = 'all 0.5s';
+
     })
 }
+
+/* board */
+/* 지역td 선택시 bg/color 바뀌게 */
+let boardSelectRegion = document.getElementsByClassName('region');
+for(let i=0; i<boardSelectRegion.length; i++){
+    boardSelectRegion[i].addEventListener('click', function(event){
+        for(let j=0; j<boardSelectRegion.length; j++){
+            if(boardSelectRegion[j].style.background = 'cornflowerblue'){
+                boardSelectRegion[j].style.background = 'white';
+                boardSelectRegion[j].style.color = 'black';
+            }
+        }
+        boardSelectRegion[i].style.background = 'cornflowerblue';
+        boardSelectRegion[i].style.color = 'white'; 
+    })
+    console.log(boardSelectRegion[0].style.background);
+}
+
+/* 각 table td 선택시 옆 div 바뀌게 */
