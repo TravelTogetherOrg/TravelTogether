8285a352282c938774d329f8d328362af94f1a7a
Hoony <111875357+Hoonyyyy@users.noreply.github.com>
2023-07-19 12:24:23 +0900

    Update FestivalController.java

--- TravelTogether/src/main/java/com/traveltogether/view/festival/FestivalController.java
+++ TravelTogether/src/main/java/com/traveltogether/view/festival/FestivalController.java
@@ -1,32 +1,23 @@
 package com.traveltogether.view.festival;
 
-<<<<<<< HEAD
-=======
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 import javax.servlet.http.HttpSession;
 
->>>>>>> KHS
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
->>>>>>> KHS
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.traveltogether.biz.festival.FestivalVO;
 import com.traveltogether.biz.festival.impl.FestivalServiceimpl;
-<<<<<<< HEAD
-=======
 import com.traveltogether.biz.member.MemberVO;
->>>>>>> KHS
 
 @Controller
 @SessionAttributes("festival")
@@ -36,13 +27,6 @@ public class FestivalController {
 	private FestivalServiceimpl festivalService;
 	
 	
-<<<<<<< HEAD
-	
-	@RequestMapping("/getFestival.do")
-	public String getFestival(FestivalVO vo, Model model) {
-		
-		model.addAttribute("festival", festivalService.getFestival(vo));
-=======
 	@RequestMapping(value = "/festivalLike.do", method = RequestMethod.GET)
 	public String getFestivalLike(FestivalVO vo, HttpSession session, Model model) {
 	    String encodedFestivalName = URLEncoder.encode(vo.getFestival_name(), StandardCharsets.UTF_8);
@@ -77,26 +61,18 @@ public class FestivalController {
     	model.addAttribute("festivalCount", festivalService.getLikeCount(vo));
 		model.addAttribute("festival", festivalService.getFestival(vo));
 		
->>>>>>> KHS
 		return "views/festivalDetail.jsp";
 		
 	}
 	
-<<<<<<< HEAD
-	@RequestMapping("/getFestivalList_Month.do")
-	public String getFestivalList_Month(FestivalVO vo, Model model){
-=======
 	
 	@RequestMapping("/getFestivalList_Month.do")
 	public String getFestivalList_Month(FestivalVO vo, Model model){
 	
->>>>>>> KHS
 		model.addAttribute("festivalList", festivalService.getFestivalList_Month(vo));
 		return "views/festivalList.jsp";
 	}
 	
-<<<<<<< HEAD
-=======
 	@RequestMapping("main.do")
 	public String main(FestivalVO vo, Model model){
 	
@@ -107,5 +83,4 @@ public class FestivalController {
 	}
 	
 	
->>>>>>> KHS
 }
