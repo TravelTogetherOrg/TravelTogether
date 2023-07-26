7f5611acc1e0792e3bb2e11abe4d063ee8980efb
ChoYool <97028346+kimchoyool@users.noreply.github.com>
2023-07-26 22:10:30 +0900

    Delete TravelTogether directory

--- TravelTogether/src/main/java/com/traveltogether/biz/chat/ChatListService.java
+++
@@ -1,9 +0,0 @@
-package com.traveltogether.biz.chat;
-
-public interface ChatListService {
-
-	void insertChat(ChatListDTO dto);
-	void getChatList(ChatListDTO dto);
-	void deleteChat(ChatListDTO dto);
-	
-}
